03895ab3654d0bacb6a6cadab46cede23a24741a
jacob <jacobharv00@gmail.com>
2022-09-05 12:58:06 -0600

    feat: get stack navigator back button to work

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import {View, StyleSheet, Text} from 'react-native'
+import { View, StyleSheet, Text, Button } from 'react-native'
 
 const SigninScreen = () => {
-  return <Text style={{fontSize: 48}}>SigninScreen</Text>
+  return <Text style={{ fontSize: 48 }}>Signin Screen</Text>
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create( {
 
-})
+} )
 
 export default SigninScreen
